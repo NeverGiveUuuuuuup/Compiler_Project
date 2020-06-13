af77b4a4633806be7cff22b48079a419a647aff0
Yuesefus <54543042+Yuesefus@users.noreply.github.com>
2020-06-13 21:31:13 +0800

    Update solution2.cc

--- project2/solution/solution2.cc
+++ project2/solution/solution2.cc
@@ -1126,12 +1126,13 @@ public:
 };
 
 // todo: 从右边值节点开始递归求微分。
-Expr find_dx(AST *RHS){
+Expr find_dx(AST *RHS){//not completed
     if(child.size()==3){
         Expr ch1 = Expr(child[0].ep);Expr ch2 = Expr(child[2].ep);
         if(child[1].str=="+")return Binary::make(data_type, BinaryOpType::Add,find_dx(ch1),find_dx(ch2));
         if(child[1].str=="-")return Binary::make(data_type, BinaryOpType::Sub,find_dx(ch1),find_dx(ch2));
-        if(child[1].str=="*")return Binary::make(data_type, BinaryOpType::Add,Binary::make(data_type,BinaryOpType::Mul,find_dx(ch1),ch2),Binary::make(data_type, BinaryOpType::Mul, ch1,find_dx(ch2)));
+        if(child[1].str=="*")return Binary::make(data_type, BinaryOpType::Add,
+            Binary::make(data_type,BinaryOpType::Mul,find_dx(ch1),ch2),Binary::make(data_type, BinaryOpType::Mul, ch1,find_dx(ch2)));
         if(child[1].str=="/")return Binary::make(data_type, BinaryOpType::Div,find_dx(ch1),ch2);
         }
 }
